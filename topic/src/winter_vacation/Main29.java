1f959a87d4f6633413030e94b8ec0ed883e5f621
RenXin <794521557@qq.com>
2020-02-09 20:20:39 +0800

    通配符匹配

---
+++ topic/src/winter_vacation/Main29.java
@@ -0,0 +1,47 @@
+package winter_vacation;
+
+/**
+ * @PackageName: winter_vacation
+ * @ClassName: Main29
+ * @Description:
+ * 给定一个字符串 (s) 和一个字符模式 (p) ，实现一个支持 '?' 和 '*' 的通配符匹配。
+ * '?' 可以匹配任何单个字符。
+ * '*' 可以匹配任意字符串（包括空字符串）。
+ * 两个字符串完全匹配才算匹配成功。
+ *
+ * 说明:
+ * s 可能为空，且只包含从 a-z 的小写字母。
+ * p 可能为空，且只包含从 a-z 的小写字母，以及字符 ? 和 *。
+ * @author: 呆呆
+ * @date: 2020/2/9
+ */
+public class Main29 {
+    public boolean isMatch(String s, String p) {
+        boolean[][] value = new boolean[p.length()+1][s.length()+1];
+        value[0][0] = true;
+        for(int i = 1;i <= s.length();i++){
+            value[0][i] = false;
+        }
+        for(int i = 1;i <= p.length(); i++){
+            if(p.charAt(i-1) == '*'){
+                value[i][0] = value[i-1][0];
+                for(int j = 1;j <= s.length(); j++){
+                    value[i][j] = (value[i][j-1] || value[i-1][j]);
+                }
+            }else if(p.charAt(i-1) == '?'){
+                value[i][0] = false;
+                for(int j = 1;j <= s.length(); j++){
+                    value[i][j] = value[i-1][j-1];
+                }
+            }else {
+                value[i][0] = false;
+                for(int j = 1;j <= s.length(); j++){
+                    value[i][j] = s.charAt(j-1) == p.charAt(i-1) && value[i-1][j-1];
+                }
+            }
+
+        }
+        return value[p.length()][s.length()];
+
+    }
+}
